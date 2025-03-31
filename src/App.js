dcfb731bbdd097bbb5fcb3f827a3d0d15652fe8f
AHSharifi <mr.amirhossein88@gmail.com>
2025-03-31 04:17:12 +0330

    panel menu

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./assets/styles/App.css";
 import Main from "./pages/layouts/main";
 import Home from "./pages/home/home";
 import Panel from "./pages/layouts/panel";
+import Dashboard from "./pages/admin/dashboard";
+import Teams from "./pages/admin/teams";
 
 function App() {
   return (
@@ -11,7 +13,14 @@ function App() {
         <Route path="/" element={<Main />}>
           <Route index element={<Home />} />
         </Route>
-        <Route path="/panel/" element={<Panel />}></Route>
+        <Route path="/panel" element={<Panel />}>
+          <Route index element={<Dashboard />} />
+          <Route path="team" element={<Teams />} />
+          <Route path="transactions" element={<Teams />} />
+          <Route path="history" element={<Teams />} />
+          <Route path="offers" element={<Teams />} />
+          <Route path="settings" element={<Teams />} />
+        </Route>
         {/* <Route path="/*" element={} /> */}
       </Routes>
     </BrowserRouter>
--- src/components/panelMenu.js
+++ src/components/panelMenu.js
@@ -1,6 +1,66 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function PanelMenu() {
+  const [activeLink, setActiveLink] = useState(0);
+  const location = useLocation();
+  const navLinks = [
+    {
+      path: "/panel/",
+      icon1: "bi-house",
+      icon2: "bi-house-fill",
+      text: "داشبورد",
+      exact: true,
+    },
+    {
+      path: "/panel/team",
+      icon1: "bi-people",
+      icon2: "bi-people-fill",
+      text: "تیم من",
+      exact: true,
+    },
+    {
+      path: "/panel/transactions",
+      icon1: "bi-cash",
+      icon2: "bi-cash-stack",
+      text: "تراکنش ها",
+      exact: true,
+    },
+    {
+      path: "/panel/history",
+      icon1: "bi-clock-history",
+      icon2: "bi-clock-history",
+      text: "تاریخچه بازی ها",
+      exact: true,
+    },
+    {
+      path: "/panel/offers",
+      icon1: "bi-star",
+      icon2: "bi-star-fill",
+      text: "پیشنهاد ویژه",
+      exact: true,
+    },
+    {
+      path: "/panel/settings",
+      icon1: "bi-gear",
+      icon2: "bi-gear-fill",
+      text: "تنظیمات",
+      exact: true,
+    },
+  ];
+
+  useEffect(() => {
+    const activeIndex = navLinks.findIndex((link) =>
+      link.exact
+        ? location.pathname === link.path
+        : location.pathname.startsWith(link.path)
+    );
+
+    if (activeIndex !== -1) {
+      setActiveLink(activeIndex);
+    }
+  }, [location.pathname]);
+
   return (
     <header>
       <div className="profile">
@@ -8,10 +68,27 @@ function PanelMenu() {
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKV5tUbnX6wSXB8VxeVu-KWkuEltLRqGlxzQ&s"
           alt=""
         />
-        <h1>امیرحسین</h1>
+        <h1>Amir</h1>
       </div>
       <nav>
-        <Link to="">داشبورد</Link>
+        {navLinks.map((link, index) => (
+          <Link
+            to={link.path}
+            key={index}
+            className={activeLink === index ? "active" : ""}
+            onClick={() => setActiveLink(index)}
+          >
+            <i className={`bi ${link.icon1}`}></i>
+            <i className={`bi ${link.icon2}`}></i>
+            <p>{link.text}</p>
+            <div className="top">
+              <div></div>
+            </div>
+            <div className="bot">
+              <div></div>
+            </div>
+          </Link>
+        ))}
       </nav>
     </header>
   );
