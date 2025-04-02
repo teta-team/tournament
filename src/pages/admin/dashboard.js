c432a4c316e75fca8ed44dfd2bcf512f9dda410c
AHSharifi <mr.amirhossein88@gmail.com>
2025-04-02 21:32:27 +0330

    update

--- src/pages/admin/dashboard.js
+++ src/pages/admin/dashboard.js
@@ -75,18 +75,21 @@ function Dashboard() {
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKV5tUbnX6wSXB8VxeVu-KWkuEltLRqGlxzQ&s"
               alt=""
             />
+            <h5>Amir</h5>
           </div>
           <div className="player">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKV5tUbnX6wSXB8VxeVu-KWkuEltLRqGlxzQ&s"
               alt=""
             />
+            <h5>Amir</h5>
           </div>
           <div className="player">
             <img
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKV5tUbnX6wSXB8VxeVu-KWkuEltLRqGlxzQ&s"
               alt=""
             />
+            <h5>Amir</h5>
           </div>
         </div>
       </div>
