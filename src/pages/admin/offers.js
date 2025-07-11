91addc0a7824e5e792d8e0f996fb445c71518775
AHSharifi <mr.amirhossein88@gmail.com>
2025-07-11 14:29:49 +0330

    add animations

--- src/pages/admin/offers.js
+++ src/pages/admin/offers.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import subs from "../../data/subs.json"
 
 function Offers() {
@@ -7,6 +8,21 @@ function Offers() {
     process.env.REACT_APP_SUB3_PRICE,
     process.env.REACT_APP_SUB4_PRICE,
   ];
+
+  useEffect(() => {
+    const items = document.querySelectorAll(
+      ".offers .sub"
+    );
+
+    items.forEach((item, index) => {
+      item.style.opacity = "0";
+      setTimeout(() => {
+        item.style.animation = "faderight .5s ease";
+        item.style.opacity = "1";
+      }, 100 * index);
+    });
+  }, []);
+
   return (
     <div className="offers">
       {subs.map((sub, index) => (
